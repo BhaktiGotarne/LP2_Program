59f4e35109b8da8e814789fa524fa790c091eafa
Bhakti Gotarne <95295449+BhaktiGotarne@users.noreply.github.com>
2023-05-30 01:55:06 +0530

    Update 7.py

--- practicals/7.py
+++ practicals/7.py
@@ -1,53 +1,10 @@
-# def selectionSort(arr):
-#     for i in range(len(arr)):
-#         min = float('-inf')
-#         for j in range(i + 1, len(arr)):
-#             if arr[i] > arr[j]:
-#                 arr[i],arr[j] = arr[j], arr[i]
-#     return arr
-    
-# print(selectionSort([89,56,45,34,65,76]))
-
-
-# def selectionSort(arr):
-#     for i in range(len(arr)):
-#         min=float('-inf')
-#         for j in range(i+1,len(arr)):
-#             if arr[i]>arr[j]:
-#                 arr[i],arr[j]=arr[j],arr[i]
-#                 return arr
-# print(selectionSort([12,14,87,45,23,90]))
-
-
-
-# 
-
-# def selectionSort(arr):
-#     for i in range(len(arr)):
-#         min=float('-inf')
-#         for j in range(i+1,len(arr)):
-#             if arr[i]>arr[j]:
-#                 arr[i],arr[j]=arr[j],arr[i]
-#                 return arr
-# print(selectionSort([12,14,45,36,87]))            
-
-
-# def selectionSort(arr):
-#     for i in range(len(arr)):
-#         min=float('-inf')
-#         for j in range(i+1,len(arr)):
-#             if arr[i]>arr[j]:
-#                 arr[i],arr[j]=arr[j],arr[i]
-#                 return arr
-# print(selectionSort([1,9,0,4,5]))            
-
-
-
 def selectionSort(arr):
     for i in range(len(arr)):
-        min=float('-inf')
-        for j in range(i+1,len(arr)):
-            if arr[i]>arr[j]:
-                arr[i],arr[j]=arr[j],arr[i]
-                return arr
-print(selectionSort([23,45,56,78,56,12]))
+        min_index = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+print(selectionSort([23, 45, 56, 78, 56, 12]))
